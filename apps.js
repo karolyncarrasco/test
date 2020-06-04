fd61d82b939393f19add35e6ac32bbeab6321e91
karolyncarrasco <karolyncarrascosandoval@gmail.com>
2020-06-04 00:14:36 -0500

    hello

--- apps.js
+++ apps.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hola Mundo');
-});
+}); //hola
 
 server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
